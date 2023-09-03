360a92c5644aa581bbc8e6287afdcaf459d30085
kintsugiJONES <gilbertpolanco42@gmail.com>
2023-09-03 12:40:16 -0400

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 
+import { Button } from '@/components/ui/button'
+import { ChevronRightIcon } from "@radix-ui/react-icons"
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <h1>hi</h1>
+       <Button variant='ghost' size='lg'>
+       <ChevronRightIcon className="h-4 w-4" />
+
+       </Button>
+
 
     </main>
   )
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -16,15 +16,15 @@ export  default function  ModeToggle() {
   const { setTheme } = useTheme()
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <DropdownMenu >
+      <DropdownMenuTrigger asChild >
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
